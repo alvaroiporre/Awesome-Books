50f5860bebb7fac8529484aecee759d08ae61fcf
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-04-24 16:14:47 -0400

    Add index.js file

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let books = localStorage.books? JSON.parse(localStorage.books):[];
+let id = localStorage.id? localStorage.id: 0;
+
+
+const booksContainer = document.getElementById('books');
+const inputTitle = document.getElementById('input-title');
+const inputAuthor = document.getElementById('input-author');
+const btnAdd = document.getElementById('btn-add');
+
+function updateBooks() {
+    booksContainer.innerHTML = '';
+    books.forEach((book) => {
+        booksContainer.innerHTML += `
+            <div class="book" id="${book.id}">
+                <p class="title">${book.title}</p>
+                <p class="author">${book.author}</p>
+                <input class="btn-remove" onclick=remove(${book.id}) type="button" value="Remove">
+            </div>
+        `;
+    });
+}
+
+updateBooks();
+
+btnAdd.addEventListener('click', (event) => {
+    event.preventDefault();
+    books.push({
+        id: id++,
+        title: inputTitle.value,
+        author: inputAuthor.value
+    });
+    inputTitle.value = '';
+    inputAuthor.value = '';
+    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem('id', id);
+    updateBooks();
+});
