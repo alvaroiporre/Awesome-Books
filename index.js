93027d0b24e63b1ef659c2a89dd95eaf60ad892e
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-04-25 17:01:47 -0400

    Fix linters

--- index.js
+++ index.js
@@ -5,66 +5,58 @@ const inputTitle = document.getElementById('input-title');
 const inputAuthor = document.getElementById('input-author');
 const btnAdd = document.getElementById('btn-add');
 
+function Book(ids, title, author) {
+  this.id = ids;
+  this.title = title;
+  this.author = author;
+}
+
 class Books {
-    constructor() {
-        this.bookList = localStorage.books? JSON.parse(localStorage.books).bookList : [];
-    }
+  constructor() {
+    this.bookList = localStorage.books ? JSON.parse(localStorage.books) : [];
+  }
 
-    updateBooks() {
-        booksContainer.innerHTML = '';
-        this.bookList.forEach((item) => {
-          booksContainer.innerHTML += `
-                  <div class="book book-1" id="${item.id}">
-                      <p class="title">${item.title}</p>
-                      <p class="title">by</p>
-                      <p class="author">${item.author}</p>
-                      <span class="span-remove"><input class="btn-remove" onclick=books.remove(${item.id}) type="button" value="Remove"></span>
-                  </div>
-              `;
-        });
+  updateBooks() {
+    booksContainer.innerHTML = '';
+    for (let i = 0; i < this.bookList.length; i += 1) {
+      booksContainer.innerHTML += `
+        <div class="book book-${i % 2}" id="${this.bookList[i].id}">
+            <p class="title">${this.bookList[i].title}</p>
+            <p class="title">by</p>
+            <p class="author">${this.bookList[i].author}</p>
+            <span class="span-remove"><input class="btn-remove" onclick=books.remove(${this.bookList[i].id}) type="button" value="Remove"></span>
+        </div>
+        `;
     }
+  }
 
-    addBook(book) {
-        this.bookList.push(book);
-        this.saveBooks();
-        this.updateBooks();
-    }
+  addBook(book) {
+    this.bookList.push(book);
+    this.saveBooks();
+    this.updateBooks();
+  }
 
-    saveBooks() {
-        localStorage.setItem('books', JSON.stringify(books));
-    }
+  saveBooks() {
+    localStorage.setItem('books', JSON.stringify(this.bookList));
+  }
 
-    remove(ids) {
-        console.log('remove', ids);
-        this.bookList = this.bookList.filter((element) => element.id !== ids);
-        console.log('list', this.bookList);
-        this.saveBooks();
-        this.updateBooks();
-    }
+  remove(ids) {
+    this.bookList = this.bookList.filter((element) => element.id !== ids);
+    this.saveBooks();
+    this.updateBooks();
+  }
 }
 
-class Book {
-    constructor(ids, title, author) {
-        this.id = ids;
-        this.title = title;
-        this.author = author;
-    }
-}
-
-let books = new Books();
+const books = new Books();
 books.updateBooks();
 
-btnAdd.addEventListener('click', (event) => {
-    let newBook = new Book(id, inputTitle.value, inputAuthor.value)
-    books.addBook(newBook);    
-    
-    id += 1;
-    localStorage.setItem('id', id);
+btnAdd.addEventListener('click', () => {
+  const newBook = new Book(id, inputTitle.value, inputAuthor.value);
+  books.addBook(newBook);
 
-    inputTitle.value = '';
-    inputAuthor.value = '';
-  
+  id += 1;
+  localStorage.setItem('id', id);
+
+  inputTitle.value = '';
+  inputAuthor.value = '';
 });
-
-
-
