4c353fdeb3f6250e2b24700c8e59d00526c4e972
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-04-26 12:28:05 -0400

    Add navbar and SPA feature

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let id = localStorage.id ? Number(localStorage.id) : 0;
 
 const booksContainer = document.getElementById('books');
+const addContainer = document.getElementById('add-book');
+const contactContainer = document.getElementById('contact');
+
 const inputTitle = document.getElementById('input-title');
 const inputAuthor = document.getElementById('input-author');
 const btnAdd = document.getElementById('btn-add');
+
+const menuList = document.getElementById('menu-list');
+const menuAdd = document.getElementById('menu-add');
+const menuContact = document.getElementById('menu-contact');
+
 
 function Book(ids, title, author) {
   this.id = ids;
@@ -48,7 +56,6 @@ class Books {
 }
 
 const books = new Books();
-books.updateBooks();
 
 btnAdd.addEventListener('click', () => {
   const newBook = new Book(id, inputTitle.value, inputAuthor.value);
@@ -60,3 +67,31 @@ btnAdd.addEventListener('click', () => {
   inputTitle.value = '';
   inputAuthor.value = '';
 });
+
+function hide() {
+    if(!booksContainer.classList.contains('hide')) booksContainer.classList.add('hide');
+    if(!addContainer.classList.contains('hide')) addContainer.classList.add('hide');
+    if(!contactContainer.classList.contains('hide')) contactContainer.classList.add('hide');
+}
+
+function showMain() {
+    hide();
+    books.updateBooks();
+    booksContainer.classList.remove('hide');
+}
+
+menuList.addEventListener('click', () => {
+    showMain();
+});
+
+menuAdd.addEventListener('click', () => {
+    hide();
+    addContainer.classList.remove('hide');
+});
+
+menuContact.addEventListener('click', () => {
+    hide();
+    contactContainer.classList.remove('hide');
+});
+
+showMain();
