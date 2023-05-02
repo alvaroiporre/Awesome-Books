6c153d0c95daecb5879d967f4c60c99f8bd1e684
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-05-02 09:13:29 -0400

    Change functions to arrow

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Book } from './Book.js';
+
 let id = localStorage.id ? Number(localStorage.id) : 0;
 
 const booksContainer = document.getElementById('books');
@@ -12,49 +14,46 @@ const menuList = document.getElementById('menu-list');
 const menuAdd = document.getElementById('menu-add');
 const menuContact = document.getElementById('menu-contact');
 
-function Book(ids, title, author) {
-  this.id = ids;
-  this.title = title;
-  this.author = author;
-}
+
 
 class Books {
-  constructor() {
+  constructor(booksContainer) {
     this.bookList = localStorage.books ? JSON.parse(localStorage.books) : [];
+    this.booksContainer = booksContainer;
   }
 
-  updateBooks() {
-    booksContainer.innerHTML = '';
+  updateBooks = () => {
+    this.booksContainer.innerHTML = '';
     for (let i = 0; i < this.bookList.length; i += 1) {
-      booksContainer.innerHTML += `
+      this.booksContainer.innerHTML += `
         <div class="book book-${i % 2}" id="${this.bookList[i].id}">
             <p class="title">${this.bookList[i].title}</p>
             <p class="title">by</p>
             <p class="author">${this.bookList[i].author}</p>
-            <span class="span-remove"><input class="btn-remove" onclick=books.remove(${this.bookList[i].id}) type="button" value="Remove"></span>
+            <span class="span-remove"><input class="btn-remove" onclick=remove(${this.bookList[i].id}) type="button" value="Remove"></span>
         </div>
         `;
     }
   }
 
-  addBook(book) {
+  addBook = (book) => {
     this.bookList.push(book);
     this.saveBooks();
     this.updateBooks();
   }
 
-  saveBooks() {
+  saveBooks = () => {
     localStorage.setItem('books', JSON.stringify(this.bookList));
   }
 
-  remove(ids) {
+  remove = (ids) => {
     this.bookList = this.bookList.filter((element) => element.id !== ids);
     this.saveBooks();
     this.updateBooks();
   }
 }
 
-const books = new Books();
+const books = new Books(booksContainer);
 
 btnAdd.addEventListener('click', () => {
   const newBook = new Book(id, inputTitle.value, inputAuthor.value);
@@ -67,13 +66,17 @@ btnAdd.addEventListener('click', () => {
   inputAuthor.value = '';
 });
 
-function hide() {
+window.remove = (id) => {
+  books.remove(id);
+}
+
+const hide = () => {
   if (!booksContainer.classList.contains('hide')) booksContainer.classList.add('hide');
   if (!addContainer.classList.contains('hide')) addContainer.classList.add('hide');
   if (!contactContainer.classList.contains('hide')) contactContainer.classList.add('hide');
 }
 
-function showMain() {
+const showMain = () => {
   hide();
   books.updateBooks();
   booksContainer.classList.remove('hide');
